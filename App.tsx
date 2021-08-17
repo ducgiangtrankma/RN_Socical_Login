3c14d2609c74bfdfc3243a7bd0d48900e9d93b90
Giang Tran <giangtran@Giangs-MacBook-Pro.local>
2021-08-17 20:48:53 +0700

    create

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+interface AppProps {}
+const App: FC<AppProps> = ({}) => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
